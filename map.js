ca018425adbbfaca88317e5e00c0d594fd19ea5a
Svetlana Ginty <sveta.ginty@gmail.com>
2024-04-25 12:37:29 +0000

    Map

--- map.js
+++ map.js
@@ -5,15 +5,33 @@
  */
 
 // Using a for loop
+let nums = [1, 2, 3, 4, 5];
+let results = [];
+for (let num of nums) {
+  results.push(num * 2);
+}
 
+console.log(results)
 
 // Using map()
+const multByTwo = function (num) {
+  return num * 2;
+}
+const mapResults = nums.map(multByTwo);
+console.log(mapResults);
 
 
 // Simplified w/ map()
 
 
+const mapResults1 = nums.map(function (num) {return num * 2});
+console.log(mapResults1);
+
+
 // Simplfied w/ map() + arrow function
+const mapResults2 = nums.map(num =>  num * 2);
+console.log(mapResults2);
+
 
 
 // With objects:
@@ -37,3 +55,6 @@ const students = [
     skill: 'CSS'
   },
 ];
+
+const studentNameId = students.map(student => [student.name, student.id]);
+console.log(studentNameId);
